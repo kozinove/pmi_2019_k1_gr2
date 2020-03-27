ad892af314f256bb5838bdc93fb8ac4429f13525
natasergeevna <62610792+natasergeevna@users.noreply.github.com>
2020-03-27 14:19:34 +0300

    new task

---
+++ shashina/task2/classmassiv.cpp
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+class massiv
+{
+public:
+    int *arr;
+    int size;
+    massiv(int sz, int value = 0)
+    {
+        arr = new int[sz];
+        if(!arr) return;
+        size = sz;
+        for(int i = 0;i<size;i++)
+        arr[i]=value;
+    }
+
+    ~massiv()
+    {
+        delete [] arr;
+    }
+
+    massiv(const massiv &copy)
+    {
+        arr = new int[copy.size];
+        if (!arr) return;
+        size = copy.size;
+        for(int i = 0;i<copy.size;i++)
+        arr[i]=copy.arr[i];
+    }
+};
+
+void print(massiv arr)
+{
+    printf("array = [");
+    for(int i = 0;i<arr.size-1;i++)
+        printf("%d ", arr.arr[i]);
+    printf("%d]\n", arr.arr[arr.size-1]);
+}
+
+
+int main()
+{
+    massiv arr(15,46);
+    print(arr);
+    return 0;
+
+}
