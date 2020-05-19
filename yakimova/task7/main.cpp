38d380bc18be02110be843cad11a42450da3c060
yulyayakimovaa <elik@example.com>
2020-05-19 22:34:51 +0300

    add test7

---
+++ yakimova/task7/main.cpp
@@ -0,0 +1,38 @@
+#include "zoo.h"
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+
+using namespace std;
+
+void walk(const zoo& z)
+{
+	for (int i = 0; i < 10; i++)
+		z.cells[i].sound();
+}
+
+void try_add_animal(animal* a, zoo& z)
+{
+	try {
+		for (int i = 0; i < 3; i++)
+			z.cells[rand() % 10].addAnimal(a);
+	}
+	catch (const char* str) {
+		cout << "error: " << str << '\n';
+	}
+}
+
+int main() {
+
+	tiger t;
+	kangaroo k;
+	bear b;
+	zoo z;
+
+	try_add_animal(&t, z);
+	try_add_animal(&k, z);
+	try_add_animal(&b, z);
+
+	walk(z);
+	return 0;
+}
---
+++ yakimova/task7/zoo.h
@@ -0,0 +1,65 @@
+#pragma once
+#pragma once
+#include <string>
+
+class animal
+{
+public:
+	bool predator;
+	animal();
+	animal(bool _predator);
+	animal(const animal& a);
+	virtual void sound() = 0;
+	virtual bool getPred() = 0;
+};
+
+class cell
+{
+private:
+	animal* anim[2];
+	bool empty;
+	int count;
+public:
+	cell();
+	cell(const cell& c);
+	void addAnimal(animal* a);
+	int get_count() const;
+	void sound() const;
+	bool* getPredArr(bool* arr) const;
+};
+
+class tiger :public animal
+{
+public:
+	tiger();
+	tiger(const tiger& t);
+	virtual void sound();
+	virtual bool getPred();
+};
+
+class kangaroo :public animal
+{
+public:
+	kangaroo();
+	kangaroo(const kangaroo& k);
+	virtual void sound();
+	virtual bool getPred();
+};
+
+class bear :public animal
+{
+public:
+	bear();
+	bear(const bear& b);
+	virtual void sound();
+	virtual bool getPred();
+};
+
+class zoo
+{
+public:
+	cell* cells;
+	zoo();
+	zoo(const zoo& z);
+	~zoo();
+};
