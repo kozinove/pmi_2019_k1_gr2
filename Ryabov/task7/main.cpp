2d59942b42d4d8092b40220e11a50c5a88ee2e27
k-rya <61202227+k-rya@users.noreply.github.com>
2020-05-25 12:45:36 +0300

    Update main.cpp

--- Ryabov/task7/main.cpp
+++ Ryabov/task7/main.cpp
@@ -10,24 +10,37 @@ int main()
     bear be;
     bird bi;
     zeebra z;
-  
-    int num = 1;
-    int numBeast;
-    while (true)
+    Zoo.inner(1, &be);
+    Zoo.inner(1, &bi);
+    Zoo.inner(1, &z);
+    try
     {
-        cout << "1-bear,2-bird,3-zeebra";
-        cin >> numBeast;
-        cout << "Witch cage you want to use?";
-        cin >> num;
-      
-
-        if (numBeast == 1) Zoo.inner(num - 1, &be);
-        if (numBeast == 2) Zoo.inner(num - 1, &bi);
-        if (numBeast == 3) Zoo.inner(num - 1, &z);
-    
+        Zoo.inner(1, &be);
     }
-
-    Zoo.check();
-
+    catch (const char* str)
+    {
+        cout << str << '\n';
+        cout << "try again\n";
+    }
+    try
+    {
+        Zoo.inner(1, &bi);
+    }
+    catch (const char* str)
+    {
+        cout << str << '\n';
+        cout << "try again\n";
+    }
+    try
+    {
+        Zoo.inner(1, &z);
+    }
+    catch (const char* str)
+    {
+        cout << str << '\n';
+        cout << "try again\n";
+    }
+    Zoo.check;
     return 0;
+
 }
